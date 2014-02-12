236ddf5ee89fea6019a8d533c4f575640689dc69
Kenneth Chan <kenneth@prediction.io>
2014-02-12 19:49:13 +0800

    Update

--- basic/import_testdata.py
+++ basic/import_testdata.py
@@ -3,16 +3,11 @@ Import simple test data for testing getting itemrec
 """
 import predictionio
 import argparse
-import datetime
-import time
 
 MIN_VERSION = '0.5.0'
 if predictionio.__version__ < MIN_VERSION:
     err = "Require PredictionIO Python SDK version >= %s" % MIN_VERSION
     raise Exception(err)
-
-def datetime_to_ms(dt):
-    return int(time.mktime(dt.timetuple()) * 1000 + dt.microsecond / 1000)
 
 def import_testdata(app_key, api_url):
     client = predictionio.Client(app_key, 1, api_url)
@@ -22,13 +17,10 @@ def import_testdata(app_key, api_url):
     client.create_user("u2")
     client.create_user("u3")
 
-    current_time = datetime.datetime.now()
-    current_time_p1day = current_time + datetime.timedelta(1)
-    
-    client.create_item("i0", ("ta",), {"pio_startT": 12345667, "pio_endT": datetime_to_ms(current_time_p1day), "custom1": "i0c1"})
-    client.create_item("i1", ("ta",), {"pio_startT": 12345677, "pio_endT": datetime_to_ms(current_time_p1day), "custom1": "i1c1", "custom2": "i1c2"})
-    client.create_item("i2", ("ta",), {"pio_startT": 12345687, "pio_endT": datetime_to_ms(current_time), "custom2": "i2c2"})
-    client.create_item("i3", ("ta",), {"pio_startT": 12345697 })
+    client.create_item("i0", ("t1",), {"custom1": "i0c1"})
+    client.create_item("i1", ("t1","t2"), {"custom1": "i1c1", "custom2": "i1c2"})
+    client.create_item("i2", ("t1","t2"), {"custom2": "i2c2"})
+    client.create_item("i3", ("t1",))
     
     ##
     client.identify("u0")
